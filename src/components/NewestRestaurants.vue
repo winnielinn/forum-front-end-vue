2a9cfd35fba45e747242a2861177c2ae530379b0
winnielinn <yaysa903@gmail.com>
2024-05-08 11:01:04 +0800

    feat: get props in NewestRestaurants page

---
+++ src/components/NewestRestaurants.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <div class="card-header">最新餐廳</div>
+    <div class="card-body">
+      <div v-for="restaurant in restaurants" v-bind:key="restaurant.id">
+        <h4>
+          <a href="#">{{ restaurant.name }}</a>
+          <small>{{ restaurant.Category.name ?? '未分類' }}</small>
+        </h4>
+        <p>
+            {{ restaurant.description }}
+        </p>
+        {{ restaurant.createdAt }}
+        <hr />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    restaurants: {
+      type: Array, // 進來的資料型態是陣列
+      required: true, // 這組資料是必要的
+    },
+  },
+};
+</script>
+
+<style scoped>
+small {
+  font-size: 10px;
+}
+</style>
