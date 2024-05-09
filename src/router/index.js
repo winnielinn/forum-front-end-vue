8251f657bf5da0be5df4a90c229735a271fff61d
winnielinn <yaysa903@gmail.com>
2024-05-09 21:39:43 +0800

    feat: add mock data in restaurant

--- src/router/index.js
+++ src/router/index.js
@@ -38,6 +38,11 @@ const routes = [
     component: () => import('../views/RestaurantsTop.vue')
   },
   {
+    path: '/restaurants/:id',
+    name: 'restaurant',
+    component: () => import('../views/Restaurant.vue')
+  },
+  {
     path: '/users/top',
     name: 'users-top',
     component: () => import('../views/UsersTop.vue')
@@ -46,7 +51,7 @@ const routes = [
     path: '*',
     name: 'not-found',
     component: NotFound
-  }
+  },
 ]
 
 const router = new VueRouter({
