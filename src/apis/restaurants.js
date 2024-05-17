61a2ccd14676959d80038c4428e965aab5c43761
winnielinn <yaysa903@gmail.com>
2024-05-17 22:39:03 +0800

    feat: add restaurants api for restaurants page

---
+++ src/apis/restaurants.js
@@ -0,0 +1,14 @@
+import { apiHelper } from "../utils/helper";
+
+const getToken = () => localStorage.getItem("token");
+
+export default {
+  getRestaurants({ page, categoryId }) {
+    const searchParams = new URLSearchParams({ page, categoryId });
+    return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
+      headers: {
+        Authorization: `Bearer ${getToken()}`,
+      },
+    });
+  },
+};
