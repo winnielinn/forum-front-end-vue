abbf59524b2a7d274ab97ce886bd4c4cfa618fe6
winnielinn <yaysa903@gmail.com>
2024-05-20 21:19:09 +0800

    feat: add vuex

---
+++ src/store/index.js
@@ -0,0 +1,29 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    currentUser: {
+      id: -1,
+      name: "",
+      email: "",
+      image: "",
+      isAdmin: false,
+    },
+    isAuthenticated: false,
+  },
+  getters: {},
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser,
+      };
+      state.isAuthenticated = true;
+    }
+  },
+  actions: {},
+  modules: {},
+});
