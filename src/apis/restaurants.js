2428876ea6eee4d13c3a99bcef4ff6b5731265fa
winnielinn <yaysa903@gmail.com>
2024-05-23 20:48:27 +0800

    refator: api helper by using interceptors

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -1,33 +1,17 @@
 import { apiHelper } from "../utils/helper";
 
-const getToken = () => localStorage.getItem("token");
-
 export default {
   getRestaurants({ page, categoryId }) {
     const searchParams = new URLSearchParams({ page, categoryId });
-    return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    });
+    return apiHelper.get(`/restaurants?${searchParams.toString()}`);
   },
   getRestaurant ({ restaurantId }) {
-    return apiHelper.get(`/restaurants/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/${restaurantId}`)
   },
   getFeeds() {
-    return apiHelper.get("/restaurants/feeds", {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    });
+    return apiHelper.get("/restaurants/feeds");
   },
   getTops() {
-    return apiHelper.get("/restaurants/top", {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    });
+    return apiHelper.get("/restaurants/top");
   },
 };
--- src/apis/users.js
+++ src/apis/users.js
@@ -1,74 +1,43 @@
 import { apiHelper } from "../utils/helper";
 
-const getToken = () => localStorage.getItem("token");
-
 export default {
   getCurrentUser() {
-    return apiHelper.get("/get_current_user", {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get("/get_current_user");
   },
   get({ userId }) {
-    return apiHelper.get(`/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    });
+    return apiHelper.get(`/users/${userId}`);
   },
   update({ userId, formData }) {
-    return apiHelper.put(`/users/${userId}`, formData, {
-      headers: {
-        Authorization: `Bearer ${getToken()}`,
-      },
-    });
+    return apiHelper.put(`/users/${userId}`, formData);
   },
   addFavorite({ restaurantId }) {
-    return apiHelper.post(`/favorite/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.post(`/favorite/${restaurantId}`, null);
   },
   removeFavorite({ restaurantId }) {
-    return apiHelper.delete(`/favorite/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.delete(`/favorite/${restaurantId}`);
   },
   addLike({ restaurantId }) {
-    return apiHelper.post(`/like/${restaurantId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.post(`/like/${restaurantId}`, null);
   },
   removeLike({ restaurantId }) {
-    return apiHelper.delete(`/like/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.delete(`/like/${restaurantId}`);
   },
   getTopUsers() {
-    return apiHelper.get("/users/top", {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.get("/users/top");
   },
   addFollow({ userId }) {
-    return apiHelper.post(`/following/${userId}`, null, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.post(`/following/${userId}`, null);
   },
   removeFollow({ userId }) {
-    return apiHelper.delete(`/following/${userId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.delete(`/following/${userId}`);
   },
   createComment({ currentUser, text, restaurantId }) {
     return apiHelper.post(
       `/comments`,
-      { currentUser, text, restaurantId },
-      {
-        headers: { Authorization: `Bearer ${getToken()}` },
-      }
+      { currentUser, text, restaurantId }
     );
   },
   deleteComment({ commentId }) {
-    return apiHelper.delete(`/comments/${commentId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` },
-    });
+    return apiHelper.delete(`/comments/${commentId}`);
   },
 };
