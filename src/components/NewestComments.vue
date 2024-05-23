f10679d4ce56422f493c6920b0942de20892aa7a
winnielinn <yaysa903@gmail.com>
2024-05-23 20:53:23 +0800

    style: adjust css

--- src/components/NewestComments.vue
+++ src/components/NewestComments.vue
@@ -39,3 +39,19 @@ export default {
   mixins: [fromNowFilter],
 };
 </script>
+
+<style scoped>
+h2.my-4 {
+  margin-bottom: 1rem !important;
+  font-size: 18px;
+}
+
+h3 {
+  margin-bottom: 3px;
+  line-height: 1.3;
+}
+
+.blockquote-footer {
+  font-size: 12.5px;
+}
+</style>
